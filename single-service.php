bebf54de097cfc7a95282f4a01699e38881a73cf
Wilhelm Ruiz <wilhelmruiz@kairadevops.com>
2024-08-07 13:21:31 -0500

    footer corregido

--- single-service.php
+++ single-service.php
@@ -226,100 +226,7 @@
         </div>
       </section>
 
-      <!-- Page Footer-->
-      <footer class="section footer-modern context-dark footer-modern-2">
-        <div class="footer-modern-line">
-          <div class="container">
-            <div class="row row-50">
-              <div class="col-md-6 col-lg-4">
-                <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">What We Offer</span></h5>
-                <ul class="footer-modern-list d-inline-block d-sm-block wow fadeInUp">
-                  <li><a href="#">Facebook Ads</a></li>
-                  <li><a href="#">PPC Services</a></li>
-                  <li><a href="#">Email Marketing</a></li>
-                  <li><a href="#">Conversion Optimization</a></li>
-                  <li><a href="#">Lead Generation</a></li>
-                  <li><a href="#">Marketing Strategy</a></li>
-                </ul>
-              </div>
-              <div class="col-md-6 col-lg-4 col-xl-3">
-                <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">Information</span></h5>
-                <ul class="footer-modern-list d-inline-block d-sm-block wow fadeInUp">
-                  <li><a href="about-us.html">About us</a></li>
-                  <li><a href="masonry-blog.html">Latest News</a></li>
-                  <li><a href="projects.html">Our Works</a></li>
-                  <li><a href="#">FAQ</a></li>
-                  <li><a href="#">Shop</a></li>
-                  <li><a href="contacts.html">Contacts Us</a></li>
-                </ul>
-              </div>
-              <div class="col-lg-4 col-xl-5">
-                <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">Newsletter</span></h5>
-                <p class="wow fadeInRight">Sign up today for the latest news and tips</p>
-                <!-- RD Mailform-->
-                <form class="rd-form rd-mailform rd-form-inline rd-form-inline-sm oh-desktop" data-form-output="form-output-global" data-form-type="subscribe" method="post" action="bat/rd-mailform.php">
-                  <div class="form-wrap wow slideInUp">
-                    <input class="form-input" id="subscribe-form-2-email" type="email" name="email" data-constraints="@Email @Required"/>
-                    <label class="form-label" for="subscribe-form-2-email">Enter your E-mail</label>
-                  </div>
-                  <div class="form-button form-button-2 wow slideInRight">
-                    <button class="button button-sm button-icon-3 button-primary-gradient" type="submit"><span class="d-none d-xl-inline-block">Subscribe</span><span class="icon mdi mdi-telegram d-xl-none"></span></button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="footer-modern-line-2">
-          <div class="container">
-            <div class="row row-30 align-items-center">
-              <div class="col-sm-6 col-md-7 col-lg-4 col-xl-4">
-                <div class="row row-30 align-items-center text-lg-center">
-                  <div class="col-md-7 col-xl-6"><a class="brand" href="index.html"><img src="images/logo-inverse-244x78.png" alt="" width="122" height="39"/></a></div>
-                  <div class="col-md-5 col-xl-6">
-                    <div class="iso-1"><img src="images/fot-img.png" alt=""/><span class="iso-1-big">9.4k</span></div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-6 col-md-12 col-lg-8 col-xl-8 oh-desktop">
-                <div class="group-xmd group-sm-justify">
-                  <div class="footer-modern-contacts wow slideInUp">
-                    <div class="unit unit-spacing-sm align-items-center">
-                      <div class="unit-left"><span class="icon icon-24 mdi mdi-phone"></span></div>
-                      <div class="unit-body"><a class="phone" href="tel:#">+1 718-999-3939</a></div>
-                    </div>
-                  </div>
-                  <div class="footer-modern-contacts wow slideInDown">
-                    <div class="unit unit-spacing-sm align-items-center">
-                      <div class="unit-left"><span class="icon mdi mdi-email"></span></div>
-                      <div class="unit-body"><a class="mail" href="mailto:#">info@demolink.org</a></div>
-                    </div>
-                  </div>
-                  <div class="wow slideInRight">
-                    <ul class="list-inline footer-social-list footer-social-list-2 footer-social-list-3">
-                      <li><a class="icon mdi mdi-facebook" href="#"></a></li>
-                      <li><a class="icon mdi mdi-twitter" href="#"></a></li>
-                      <li><a class="icon mdi mdi-instagram" href="#"></a></li>
-                      <li><a class="icon mdi mdi-google-plus" href="#"></a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="footer-modern-line-3">
-          <div class="container">
-            <div class="row row-10 justify-content-between">
-              <div class="col-md-6"><a href="#">514 S. Magnolia St. Orlando, FL 32806</a></div>
-              <div class="col-md-auto">
-                <!-- Rights-->
-                <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>Flex</span><span>.&nbsp;</span><a href="privacy-policy.html">Privacy policy</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
+
     </div>
     <!-- Panel Thumbnail-->
     <!-- Template panel-->
@@ -372,6 +279,7 @@
         </div>
       </div>
     </div>
+    <?php include('principal/footer.php') ?>
     <!-- Global Mailform Output-->
     <div class="snackbars" id="form-output-global"> </div>
 
