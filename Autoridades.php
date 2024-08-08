3e3a52c2550a2a6822639b1dbd7372e4ed690bd3
Angelrchs <angelrchavarrias@gmail.com>
2024-08-08 10:53:23 -0500

    Create Autoridades.php
    practica 1

---
+++ Autoridades.php
@@ -0,0 +1,188 @@
+<?php include('principal/header.php') ?>
+
+  <body>
+    <div class="preloader">
+      <div class="wrapper-triangle">
+        <div class="pen">
+          <div class="line-triangle">
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+          </div>
+          <div class="line-triangle">
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+          </div>
+          <div class="line-triangle">
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="page">
+      <!-- Page Header-->
+      <?php include('principal/navbar.php') ?>
+        
+      <!-- Breadcrumbs -->
+      <section class="bg-gray-7">
+        <div class="breadcrumbs-custom box-transform-wrap context-dark bg-image gradient-overlay" style="background-image: url(images/bg-typography.jpg);">
+          <div class="container">
+            <h3 class="breadcrumbs-custom-title">Services #4</h3>
+            <div class="breadcrumbs-custom-decor"></div>
+          </div>
+        </div>
+        <div class="container">
+          <ul class="breadcrumbs-custom-path">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li class="active">Services #4</li>
+          </ul>
+        </div>
+      </section>
+      <!-- Services 2-->
+      <section class="section section-lg bg-default">
+        <div class="container">
+          <div class="row row-40 row-xl-60 justify-content-center box-ordered">
+            <div class="col-sm-6 col-lg-4">
+              <!-- Services Creative-->
+              <article class="services-creative"><a class="services-creative-figure" href="single-service.html"><img src="images/services-7-370x230.jpg" alt="" width="370" height="230"/></a>
+                <div class="services-creative-caption">
+                  <h5 class="services-creative-title"><a href="single-service.html">Pay Per Click</a></h5>
+                  <p class="services-creative-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh</p><span class="services-creative-counter box-ordered-item"></span>
+                </div>
+              </article>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+              <!-- Services Creative-->
+              <article class="services-creative"><a class="services-creative-figure" href="single-service.html"><img src="images/services-8-370x230.jpg" alt="" width="370" height="230"/></a>
+                <div class="services-creative-caption">
+                  <h5 class="services-creative-title"><a href="single-service.html">Content Marketing</a></h5>
+                  <p class="services-creative-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh</p><span class="services-creative-counter box-ordered-item"></span>
+                </div>
+              </article>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+              <!-- Services Creative-->
+              <article class="services-creative"><a class="services-creative-figure" href="single-service.html"><img src="images/services-9-370x230.jpg" alt="" width="370" height="230"/></a>
+                <div class="services-creative-caption">
+                  <h5 class="services-creative-title"><a href="single-service.html">SEO Services</a></h5>
+                  <p class="services-creative-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh</p><span class="services-creative-counter box-ordered-item"></span>
+                </div>
+              </article>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+              <!-- Services Creative-->
+              <article class="services-creative"><a class="services-creative-figure" href="single-service.html"><img src="images/services-10-370x230.jpg" alt="" width="370" height="230"/></a>
+                <div class="services-creative-caption">
+                  <h5 class="services-creative-title"><a href="single-service.html">Email Marketing</a></h5>
+                  <p class="services-creative-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh</p><span class="services-creative-counter box-ordered-item"></span>
+                </div>
+              </article>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+              <!-- Services Creative-->
+              <article class="services-creative"><a class="services-creative-figure" href="single-service.html"><img src="images/services-11-370x230.jpg" alt="" width="370" height="230"/></a>
+                <div class="services-creative-caption">
+                  <h5 class="services-creative-title"><a href="single-service.html">Creative Advertising</a></h5>
+                  <p class="services-creative-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh</p><span class="services-creative-counter box-ordered-item"></span>
+                </div>
+              </article>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+              <!-- Services Creative-->
+              <article class="services-creative"><a class="services-creative-figure" href="single-service.html"><img src="images/services-12-370x230.jpg" alt="" width="370" height="230"/></a>
+                <div class="services-creative-caption">
+                  <h5 class="services-creative-title"><a href="single-service.html">Public Relations</a></h5>
+                  <p class="services-creative-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh</p><span class="services-creative-counter box-ordered-item"></span>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Page Footer-->
+      <?php include('principal/footer.php') ?>
+                <!-- Rights-->
+                <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>Flex</span><span>.&nbsp;</span><a href="privacy-policy.html">Privacy policy</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!-- Panel Thumbnail-->
+    <!-- Template panel-->
+    <div class="layout-panel">
+      <button class="layout-panel-toggle" data-custom-toggle=".layout-panel" data-custom-toggle-disable-on-blur="true"><span></span></button>
+      <div class="layout-panel-scroll">
+        <div class="layout-panel-content">
+          <div class="layout-panel-header"><a class="button button-primary button-icon button-icon-left" href="https://www.templatemonster.com/website-templates/flex-universal-multipurpose-creative-html-website-template-88389.html"><span class="icon mdi mdi-cart-outline"></span>Buy template</a>
+            <div class="layout-panel-element"><a href="../landing/index.html"><img src="../landing/images/logo-default-244x78.png" width="122" height="39" alt=""></a></div>
+          </div>
+          <!-- Isotope Content-->
+          <div class="layout-panel-body"><a class="thumbnail-small" href="../flex-dynamics/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/dynamics.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Dynamics</p>
+                  </div></a><a class="thumbnail-small" href="../flex-flooring/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/flooring.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Flooring</p>
+                  </div></a><a class="thumbnail-small" href="../flex-library/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/library.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Library</p>
+                  </div></a><a class="thumbnail-small" href="../flex-marketing-agency/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/marketing-agency.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Marketing Agency</p>
+                  </div></a><a class="thumbnail-small" href="../flex-pizza/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/pizza.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Pizza</p>
+                  </div></a><a class="thumbnail-small" href="../flex-real-estate/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/real-estate.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Real Estate</p>
+                  </div></a><a class="thumbnail-small" href="../flex-steel-processing/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/steel-processing.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Steel Processing</p>
+                  </div></a><a class="thumbnail-small" href="../flex-transportation/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/transportation.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Transportation</p>
+                  </div></a><a class="thumbnail-small" href="../flex-service-center/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/service-center.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Service Center</p>
+                  </div></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"> </div>
+    <!--pre#debug1.debug-->
+    <!-- Javascript-->
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+    <?php include('principal/script.php') ?>
+  </body>
+</html>
