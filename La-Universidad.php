5f9283c4c06210c8e54058355078c7147a1fdeed
Angelrchs <angelrchavarrias@gmail.com>
2024-08-08 12:31:11 -0500

    Create La-Universidad.php
    modificacion contenido

---
+++ La-Universidad.php
@@ -0,0 +1,250 @@
+
+<?php include('principal/header.php') ?>
+  <body>
+    <div class="preloader">
+      <div class="wrapper-triangle">
+        <div class="pen">
+          <div class="line-triangle">
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+          </div>
+          <div class="line-triangle">
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+          </div>
+          <div class="line-triangle">
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="page">
+      <!-- Page Header-->
+      <?php include('principal/navbar.php') ?>
+      <!-- Breadcrumbs -->
+      <section class="bg-gray-7">
+        <div class="breadcrumbs-custom box-transform-wrap context-dark bg-image gradient-overlay" style="background-image: url(images/bg-typography.jpg);">
+          <div class="container">
+            <h3 class="breadcrumbs-custom-title">Tabs &amp; Accordions</h3>
+            <div class="breadcrumbs-custom-decor"></div>
+          </div>
+        </div>
+        <div class="container">
+          <ul class="breadcrumbs-custom-path">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#">Elements</a></li>
+            <li class="active">Tabs &amp; Accordions</li>
+          </ul>
+        </div>
+      </section>
+      <!-- Tabs-->
+      <section class="section section-sm section-first bg-default text-md-start">
+        <div class="container">
+        
+          <div class="row">
+            <div class="col-xl-10">
+              <h4>Sobre Nosotros</h4>
+              <p>La Universidad Nacional Autónoma de Tayacaja Daniel Hernández Morillo creada con la Ley N° 29716, cuenta con una Comisión Organizadora reconformada mediante Resolución Viceministerial N° 088-2022-MINEDU, integrada por la Dra. Isabel Esteban Robladillo, presidenta; el Dr. Andrés Olivera Chura, vicepresidente académico, y el Dr. Luis Alberto Massa Palacios, como vicepresidente de Investigación; quienes están comprometidos con el desarrollo de la primera institución universitaria de Tayacaja.</p>
+              
+      <!-- Vertical Tabs-->
+      <section class="section section-sm bg-default text-md-start">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-10">
+              <h4>NUESTRA MISION Y VISION</h4>
+              <!-- Bootstrap tabs-->
+              <div class="tabs-custom tabs-vertical tabs-line" id="tabs-2">
+                <!-- Nav tabs-->
+                <ul class="nav nav-tabs">
+                  <li class="nav-item" role="presentation"><a class="nav-link active" href="#tabs-2-1" data-bs-toggle="tab">MISION</a></li>
+                  <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-2-2" data-bs-toggle="tab">VISION</a></li>
+                  
+                </ul>
+                <!-- Tab panes-->
+                <div class="tab-content">
+                  <div class="tab-pane fade show active" id="tabs-2-1">
+                    <p>Formar profesionales competentes y éticos, con capacidad para la investigación e innovación generando conocimiento científico, tecnológico y humanístico, promoviendo el desarrollo sostenible de la región y la responsabilidad social.</p>
+                  </div>
+                  <div class="tab-pane fade" id="tabs-2-2">
+                    <p>Los peruanos acceden a una educación, que les permite desarrollar su potencial desde la primera infancia y convertirse en ciudadanos que valoran su cultura, conocen sus derechos y responsabilidades, desarrollan sus talentos y participan de manera innovadora, competitiva y comprometida en las dinámicas sociales, contribuyendo al desarrollo de sus comunidades y del país en su conjunto.</p>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Historia-->
+       <!-- Our History-->
+    <section class="section section-lg bg-gray-100 text-start section-relative section-bg-creative">
+        <div class="container">
+            <div class="row row-60 justify-content-center justify-content-xxl-between">
+                <div class="col-lg-6 col-xxl-5 position-static">
+                    <h3 class="oh-desktop"><span class="d-inline-block wow slideInUp">NUESTRA HISTORIA</h3>
+                    <div class="tabs-custom" id="tabs-5">
+                        <div class="tab-content tab-content-1">
+                            <div class="tab-pane fade" id="tabs-5-1">
+                                <h5 class="fw-normal text-transform-none text-spacing-75">Inicio</h5>
+                                <p>Fundamos  la UNAT para ayudar a nuestros Alumnos a llegar a sus futuros clientes a travez de enseñanzas tradicionales y métodos innovadores que revolucionaron la industria.</p>
+                            </div>
+                            <div class="tab-pane fade" id="tabs-5-2">
+                                <h5 class="fw-normal text-transform-none text-spacing-75">Primeros Años</h5>
+                                <p>Fortalecimos nuestros programas académicos de alta calidad que promuevan el aprendizaje y el dominio de diversos idiomas, fomentando el desarrollo de habilidades comunicativas y culturales en nuestra comunidad estudiantil; donde buscamos contribuir al crecimiento académico y personal de nuestros estudiantes, preparándolos para enfrentar los desafíos globales y fomentando la comprensión intercultural.</p>
+                            </div>
+                            <div class="tab-pane fade" id="tabs-5-3">
+                                <h5 class="fw-normal text-transform-none text-spacing-75">en Pandemia</h5>
+                                <p>Para brindarles a nuestros Alumnados más y mejores oportunidades en el mundo del aprendizaje online, nos adaptamos a las nuevas tecnologias con especialistas los cuales nos permitieron lograr mayor interaccion con nuestros alumnos.</p>
+                            </div>
+                            <div class="tab-pane fade show active" id="tabs-5-4">
+                                <h5 class="fw-normal text-transform-none text-spacing-75 wow fadeInRight">Quienes somos</h5>
+                                <p class="wow fadeInRight" data-wow-delay=".1s">El Centro de Idiomas de la UNAT, perteneciente a la Universidad Nacional Autonoma de Tayacaja, se especializa en la enseñanza de inglés,  Portugues como lenguas extranjeras y Quechua como lengua peruana. Contamos con 13 años de experiencia. Estamos aquí para responder a todas tus preguntas y guiarte en tu camino hacia el éxito lingüístico. Ya sea que estés listo para inscribirte en nuestros programas, explorar nuestras opciones o conocer más sobre nuestros servicios, nuestro equipo está disponible para ofrecerte asistencia personalizada. No dudes en ponerte en contacto con nosotros a través de los medios proporcionados y descubre cómo el Centro de Idiomas puede ser tu clave para desbloquear un mundo de oportunidades lingüísticas.</p>
+                            </div>
+                        </div>
+                        <div class="list-history-wrap">
+                            <ul class="nav list-history">
+                                <li class="list-history-item" role="presentation"><a href="#tabs-5-1" data-bs-toggle="tab">
+                                        <div class="list-history-circle"></div>2011</a></li>
+                                <li class="list-history-item" role="presentation"><a href="#tabs-5-2" data-bs-toggle="tab">
+                                        <div class="list-history-circle"></div>2015</a></li>
+                                <li class="list-history-item" role="presentation"><a href="#tabs-5-3" data-bs-toggle="tab">
+                                        <div class="list-history-circle"></div>2020</a></li>
+                                <li class="list-history-item" role="presentation"><a class="active" href="#tabs-5-4" data-bs-toggle="tab">
+                                        <div class="list-history-circle"></div>2024</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9 col-lg-6 position-static index-1">
+                    <div class="bg-image-right-1 bg-image-right-lg gradient-overlay"><img src="images/idioma/2.jpg" alt="" width="1110" height="710"/>
+                        <div class="link-play-modern"><a class="icon mdi mdi-play" data-lightgallery="item" href="https://www.youtube.com/watch?v=1UWpbtUupQQ"></a>
+                            <div class="link-play-modern-title">How we<span>Wor</span></div>
+                            <div class="link-play-modern-decor"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+      <!-- Accordion-->
+      <section class="section section-sm section-last bg-default text-md-start">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-9 col-xl-7">
+              <h4>Documentacion</h4>
+              <!-- Bootstrap collapse-->
+              <div class="card-group-custom card-group-corporate" id="accordion1" role="tablist" aria-multiselectable="false">
+                <!-- Bootstrap card-->
+                <article class="card card-custom card-corporate">
+                  <div class="card-header" id="accordion1Heading1" role="tab">
+                    <div class="card-title"><a role="button" data-bs-toggle="collapse" data-bs-parent="#accordion1" href="#accordion1Collapse1" aria-controls="accordion1Collapse1" aria-expanded="true">ESTATUTO
+                        <div class="card-arrow"></div></a>
+                    </div>
+                  </div>
+                  <div class="collapse show" id="accordion1Collapse1" role="tabpanel" aria-labelledby="accordion1Heading1" data-bs-parent="#accordion1">
+                    <div class="card-body">
+                      <p>Descargar.</p>
+                    </div>
+                  </div>
+                </article>
+                <!-- Bootstrap card-->
+                <article class="card card-custom card-corporate">
+                  <div class="card-header" id="accordion1Heading2" role="tab">
+                    <div class="card-title"><a class="collapsed" role="button" data-bs-toggle="collapse" data-bs-parent="#accordion1" href="#accordion1Collapse2" aria-controls="accordion1Collapse2" >ORGANIGRAMA
+                        <div class="card-arrow"></div></a>
+                    </div>
+                  </div>
+                  <div class="collapse" id="accordion1Collapse2" role="tabpanel" aria-labelledby="accordion1Heading2" data-bs-parent="#accordion1">
+                    <div class="card-body">
+                      <p>Descargar.</p>
+                    </div>
+                  </div>
+                </article>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Page Footer-->
+      <?php include('principal/footer.php') ?>
+      <!-- Rights-->
+                <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>Flex</span><span>.&nbsp;</span><a href="privacy-policy.html">Privacy policy</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!-- Panel Thumbnail-->
+    <!-- Template panel-->
+    <div class="layout-panel">
+      <button class="layout-panel-toggle" data-custom-toggle=".layout-panel" data-custom-toggle-disable-on-blur="true"><span></span></button>
+      <div class="layout-panel-scroll">
+        <div class="layout-panel-content">
+          <div class="layout-panel-header"><a class="button button-primary button-icon button-icon-left" href="https://www.templatemonster.com/website-templates/flex-universal-multipurpose-creative-html-website-template-88389.html"><span class="icon mdi mdi-cart-outline"></span>Buy template</a>
+            <div class="layout-panel-element"><a href="../landing/index.html"><img src="../landing/images/logo-default-244x78.png" width="122" height="39" alt=""></a></div>
+          </div>
+          <!-- Isotope Content-->
+          <div class="layout-panel-body"><a class="thumbnail-small" href="../flex-dynamics/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/dynamics.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Dynamics</p>
+                  </div></a><a class="thumbnail-small" href="../flex-flooring/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/flooring.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Flooring</p>
+                  </div></a><a class="thumbnail-small" href="../flex-library/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/library.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Library</p>
+                  </div></a><a class="thumbnail-small" href="../flex-marketing-agency/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/marketing-agency.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Marketing Agency</p>
+                  </div></a><a class="thumbnail-small" href="../flex-pizza/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/pizza.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Pizza</p>
+                  </div></a><a class="thumbnail-small" href="../flex-real-estate/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/real-estate.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Real Estate</p>
+                  </div></a><a class="thumbnail-small" href="../flex-steel-processing/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/steel-processing.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Steel Processing</p>
+                  </div></a><a class="thumbnail-small" href="../flex-transportation/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/transportation.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Transportation</p>
+                  </div></a><a class="thumbnail-small" href="../flex-service-center/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/service-center.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Service Center</p>
+                  </div></a>
+          </div>
+        </div>
+      </div><?php include('principal/footer.php') ?>
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"> </div>
+  <?php include('principal/script.php') ?>
+
+  </body>
+</html>
