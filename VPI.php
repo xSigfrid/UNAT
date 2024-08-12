e703bea08280991d96b8890488a0b502c63d5af4
Angelrchs <angelrchavarrias@gmail.com>
2024-08-12 10:09:22 -0500

    Create VPI.php

---
+++ VPI.php
@@ -0,0 +1,185 @@
+<?php include('principal/header.php') ?>
+
+
+  <body>
+    <div class="preloader">
+      <div class="wrapper-triangle">
+        <div class="pen">
+          <div class="line-triangle">
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+          </div>
+          <div class="line-triangle">
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+          </div>
+          <div class="line-triangle">
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+            <div class="triangle"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="page">
+      <!-- Page Header-->
+      <?php include('principal/navbar.php') ?>
+       
+      <!-- Breadcrumbs -->
+      <section class="bg-gray-7">
+        <div class="breadcrumbs-custom box-transform-wrap context-dark bg-image gradient-overlay" style="background-image: url(images/bg-typography.jpg);">
+          <div class="container">
+            <h3 class="breadcrumbs-custom-title">Comision Organizadora Universidad Nacional Autonoma de Tayacaja Daniel Hernandez Morillo</h3>
+            <div class="breadcrumbs-custom-decor"></div>
+          </div>
+        </div>
+       
+      </section>
+      <!-- Project Page-->
+      <section class="section section-lg bg-default text-md-start">
+        <div class="container">
+          <div class="row row-60 flex-lg-row-reverse">
+            <div class="col-lg-6 col-xl-7">
+              <div class="slick-project">
+                <!-- Slick Carousel-->
+                <div class="slick-slider carousel-parent" data-arrows="true" data-autoplay="true" data-swipe="true" data-items="1" data-child="#child-carousel-7" data-for="#child-carousel-7" data-slide-effect="true">
+                  <div class="item"><img src="images\section2/s4.jpg" alt="" width="670" height="477"/>
+                    <div class="slick-project-caption">
+                      <div class="slick-project-title">Photo #1</div>
+                    </div>
+                  </div>
+                  <div class="item"><img src="images/project-6-670x477.jpg" alt="" width="670" height="477"/>
+                    <div class="slick-project-caption">
+                      <div class="slick-project-title">Photo #2</div>
+                    </div>
+                  </div>
+                  <div class="item"><img src="images/project-7-670x477.jpg" alt="" width="670" height="477"/>
+                    <div class="slick-project-caption">
+                      <div class="slick-project-title">Photo #3</div>
+                    </div>
+                  </div>
+                  <div class="item"><img src="images/project-8-670x477.jpg" alt="" width="670" height="477"/>
+                    <div class="slick-project-caption">
+                      <div class="slick-project-title">Photo #4</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="slick-slider child-carousel" id="child-carousel-7" data-for=".carousel-parent" data-items="3" data-sm-items="3" data-md-items="4" data-lg-items="3" data-xl-items="4" data-slide-to-scroll="1">
+                  <div class="item"><img src="images/project-5-670x477.jpg" alt="" width="670" height="477"/>
+                  </div>
+                  <div class="item"><img src="images/project-6-670x477.jpg" alt="" width="670" height="477"/>
+                  </div>
+                  <div class="item"><img src="images/project-7-670x477.jpg" alt="" width="670" height="477"/>
+                  </div>
+                  <div class="item"><img src="images/project-8-670x477.jpg" alt="" width="670" height="477"/>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+            <div class="col-lg-6 col-xl-5">
+              <div class="project-page">
+                <h4>Dra. Roxani Keewing Zapata ZAPATA</h4>
+                <p>Vice Presidente de Investigacion de la universidad Nacional Autonoma de Tayacaja. </p>
+                <p>Resolucion Viseministerial N°054-2024-MINEDU, Resuelve: </p>
+                <p>Artículo 3.- Reconformar la Comisión Organizadora de la Universidad Nacional Autónoma de Tayacaja Daniel Hernández Morillo, quedando integrada por:</p>
+                <p>ROXANI KEEWONG ZAPATA, como Vicepresidenta de Investigación </p>
+                <div class="group-sm group-middle"><span class="project-page-social-title">Share</span>
+                  <div>
+                    <ul class="list-inline project-page-social-list">
+                      <li><a class="icon mdi mdi-facebook" href="#"></a></li>
+                      <li><a class="icon mdi mdi-twitter" href="#"></a></li>
+                      <li><a class="icon mdi mdi-instagram" href="#"></a></li>
+                      <li><a class="icon mdi mdi-google-plus" href="#"></a></li>
+                    </ul>
+                  </div>
+                </div><a class="button button-lg button-primary-gradient button-winona" href="https://vri.unat.edu.pe/">Pagina VPI</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <?php include('principal/footer.php') ?>
+                <!-- Rights-->
+                <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>Flex</span><span>.&nbsp;</span><a href="privacy-policy.html">Privacy policy</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!-- Panel Thumbnail-->
+    <!-- Template panel-->
+    <div class="layout-panel">
+      <button class="layout-panel-toggle" data-custom-toggle=".layout-panel" data-custom-toggle-disable-on-blur="true"><span></span></button>
+      <div class="layout-panel-scroll">
+        <div class="layout-panel-content">
+          <div class="layout-panel-header"><a class="button button-primary button-icon button-icon-left" href="https://www.templatemonster.com/website-templates/flex-universal-multipurpose-creative-html-website-template-88389.html"><span class="icon mdi mdi-cart-outline"></span>Buy template</a>
+            <div class="layout-panel-element"><a href="../landing/index.html"><img src="../landing/images/logo-default-244x78.png" width="122" height="39" alt=""></a></div>
+          </div>
+          <!-- Isotope Content-->
+          <div class="layout-panel-body"><a class="thumbnail-small" href="../flex-dynamics/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/dynamics.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Dynamics</p>
+                  </div></a><a class="thumbnail-small" href="../flex-flooring/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/flooring.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Flooring</p>
+                  </div></a><a class="thumbnail-small" href="../flex-library/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/library.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Library</p>
+                  </div></a><a class="thumbnail-small" href="../flex-marketing-agency/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/marketing-agency.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Marketing Agency</p>
+                  </div></a><a class="thumbnail-small" href="../flex-pizza/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/pizza.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Pizza</p>
+                  </div></a><a class="thumbnail-small" href="../flex-real-estate/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/real-estate.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Real Estate</p>
+                  </div></a><a class="thumbnail-small" href="../flex-steel-processing/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/steel-processing.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Steel Processing</p>
+                  </div></a><a class="thumbnail-small" href="../flex-transportation/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/transportation.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Transportation</p>
+                  </div></a><a class="thumbnail-small" href="../flex-service-center/index.html">
+                  <div class="thumbnail-small-image"><img src="images/layout-panel/service-center.jpg" alt="" width="380" height="280"></div>
+                  <div class="thumbnail-small-caption">
+                    <p class="thumbnail-small-header">Service Center</p>
+                  </div></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"> </div>
+    <!--pre#debug1.debug-->
+    <!-- Javascript-->
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+    <?php include('principal/script.php') ?>
+  </body>
+</html>
