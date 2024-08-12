61f445f665501e892bf070551130d730a63f1dfc
Angelrchs <angelrchavarrias@gmail.com>
2024-08-12 15:08:11 -0500

    Create footer.php

---
+++ GestionAmbiental/principal/footer.php
@@ -0,0 +1,50 @@
+<footer class="section footer-modern context-dark footer-modern-2">
+    <div class="footer-modern-line">
+        <div class="container">
+            <div class="row row-50">
+                <div class="col-md-6 col-lg-4">
+                    <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">Contacto</span></h5>
+                    <ul class="footer-modern-list d-inline-block d-sm-block wow fadeInUp">
+                        <li><a href="https://www.facebook.com/unat.oficial?mibextid=ZbWKwL">Facebook oficial</a></li>
+                        <li><a href="#">Nuestro Servicio</a></li>
+                        <li><a href="#">Email UNAT</a></li>
+                        
+                    </ul>
+                    <br>
+                    <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">Trabaja con Nostros</span></h5>
+                    <ul class="footer-modern-list d-inline-block d-sm-block wow fadeInUp">
+                        <li><a href="#">Bolsa de Trabajo</a></li>
+                        <li><a href="#">Convocatorias</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-lg-4 col-xl-3">
+                    <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">Acceso Directos</span></h5>
+                    <ul class="footer-modern-list d-inline-block d-sm-block wow fadeInUp">
+                        <li><a href="https://sisacad.unat.edu.pe/zetunat_amazon/zet/index.php">Intranet</a></li>
+                        <li><a href="#">Centro de Idiomas</a></li>
+                        <li><a href="#">Cepre</a></li>
+                        <li><a href="https://www.transparencia.gob.pe/enlaces/pte_transparencia_enlaces.aspx?id_entidad=18675">Transparencia</a></li>
+                        <li><a href="https://vri.unat.edu.pe/">Investigacion</a></li>
+                        <li><a href="#">contactanos</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-xl-5">
+                    <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">Ubicación</span></h5>
+                    <p class="wow fadeInRight"> Jr. Bolognesi N° 418, Pampas</p>
+
+                    <br>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15606.35853184637!2d-75.18945279999998!3d-12.071731199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2spe!4v1722947781072!5m2!1ses!2spe" width="470" height="170" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-modern-line-3">
+        <div class="container">
+            <div class="text-center row row-10 justify-content-between">
+                <div class="col-md-12"><a href="#">Copyright © 2024 - Universidad Nacional Autonoma de Tayacaja Daniel Hernandez Morillo</a></div>
+
+            </div>
+        </div>
+    </div>
+</footer>
