ffdd2d10d05d1260d008cc295694e28a9956933e
Angelrchs <angelrchavarrias@gmail.com>
2024-09-11 19:28:48 -0500

    Update nosotros.php
    quitando carga triangiulo

--- nosotros.php
+++ nosotros.php
@@ -1,38 +1,6 @@
 <?php include('principal/header.php') ?>
 <body>
-    <div class="preloader">
-      <div class="wrapper-triangle">
-        <div class="pen">
-          <div class="line-triangle">
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-          </div>
-          <div class="line-triangle">
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-          </div>
-          <div class="line-triangle">
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-            <div class="triangle"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    
     <div class="page">
       <!-- Page Header-->
       <?php include('principal/navbar.php') ?>
